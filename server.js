95f34a3fab1eb8591bcc4f486793873d951e0b21
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2024-03-06 23:14:14 +0100

    w

--- server.js
+++ server.js
@@ -1,39 +1,42 @@
 const express = require('express');
 const { exec } = require('child_process');
+
 const app = express();
 const port = 3000;
 
-let containerId = ''; // Variable para almacenar el ID del contenedor
+const cors = require('cors');
+app.use(cors()); // Esto permite solicitudes de cualquier origen. Ajusta según sea necesario.
 
 app.use(express.json());
 
-// Ruta para encender el canal (ya existente)
-app.post('/encender-canal', (req, res) => {
-  exec('sudo docker run -d -p 8081:80 ghcr.io/martinbjeldbak/acestream-http-proxy', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return res.status(500).send({ message: 'Error al encender el canal' });
-    }
-    containerId = stdout.trim(); // Almacena el ID del contenedor
-    res.send({ message: 'Canal encendido con éxito', containerId: stdout.trim() });
-  });
-});
+let containerId = ''; // Variable para almacenar el ID del contenedor
 
-// Nueva ruta para apagar el canal
-app.post('/apagar-canal', (req, res) => {
-  if (!containerId) {
-    return res.status(400).send({ message: 'No hay un canal para apagar' });
-  }
-  exec(`sudo docker stop ${containerId}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return res.status(500).send({ message: 'Error al apagar el canal' });
-    }
-    res.send({ message: 'Canal apagado con éxito' });
-    containerId = ''; // Limpia el ID del contenedor después de detenerlo
+app.post('/encender-canal', (req, res) => {
+    exec('sudo docker run -d -p 8081:80 ghcr.io/martinbjeldbak/acestream-http-proxy', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return res.status(500).send({ message: 'Error al encender el canal' });
+      }
+      containerId = stdout.trim(); // Almacena el ID del contenedor
+      res.send({ message: 'Canal encendido con éxito', containerId: stdout.trim() });
+    });
   });
-});
+
+app.post('/apagar-canal', (req, res) => {
+    if (!containerId) {
+      return res.status(400).send({ message: 'No hay un canal para apagar' });
+    }
+    exec(`sudo docker stop ${containerId}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return res.status(500).send({ message: 'Error al apagar el canal' });
+      }
+      res.send({ message: 'Canal apagado con éxito' });
+      containerId = ''; // Limpia el ID del contenedor después de detenerlo
+    });
+  });
+  
 
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor escuchando en puerto ${port}`);
 });
