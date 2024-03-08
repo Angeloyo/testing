5104b92d31389b27889afe74f8098dee1d179321
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2024-03-08 14:23:44 +0100

    w

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { exec } = require('child_process');
 
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./canales.db');
+const db = new sqlite3.Database('./database.db');
 
 const app = express();
 const port = 3000;
